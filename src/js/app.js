ca73e9d4b39de47df5d9a801e104acb69d1b7865
Parvin-Gasimli <parvinzga@code.edu.az>
2023-01-14 01:42:21 -0800

    #add git husky and prettier husky

---
+++ src/js/app.js
@@ -0,0 +1,59 @@
+"use strict";
+let name = "konwnoname";
+surname = "mamedov";
+age = 432;
+test = "demo";
+
+var ok = "err";
+(yerar = 2022), (cumusd = "cox baxma"), (ferid = "gulumseyir");
+
+var a = prompt("enter the surname", "salamlar");
+document.write(a);
+
+var num1 = Number(prompt("enter the num"));
+var num2 = Number(prompt("entert the num2"));
+if (typeof num1 !== Number && typeof num2 !== Number) {
+  console.log("err");
+} else {
+  console.log(num1 + num2);
+}
+
+let a = Number(prompt("enini daxil edin"));
+let b = Number(prompt("uzunlugunu daxil edin"));
+let Perimetr = Number(2 * (a + b));
+let Sahe = Number(a * b);
+console.log("Perimet", Perimetr);
+console.log("Sahe", Sahe);
+console.log(2 * (a + b));
+alert("hello guys");
+
+var poker = parseInt(Math.random() * 5);
+if (poker == 2) {
+  console.log("winner");
+}
+console.log(Math.abs(-5));
+
+var a = 10;
+console.log(a++);
+
+let num = Number(prompt("1-ci ededi daxil edin"));
+var num2 = Number(prompt("2-ci ededi daxil edin"));
+
+if (num > 0 && num2 > 0) {
+  console.log(num + num2);
+} else {
+  console.log("duzgun deyk");
+}
+
+var a = 50;
+
+var b = "50";
+
+// console.log(a === b);
+// console.log(a == b);
+// console.log(a !== b);
+
+var a = "ZauR";
+var b = "ZAur";
+
+a < 10 || console.log("not defiend");
